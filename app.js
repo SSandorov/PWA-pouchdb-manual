3824963993ea6e47f585f32996c63db29bc27ce4
ssandorov <49904591+SSandorov@users.noreply.github.com>
2022-07-29 17:33:44 +0200

    Tarea 3

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const db = new PouchDB('mensajes');
 const remoteCouch = false;
 
 // Objeto a grabar en base de datos
-let mensaje = {
+let mensaje1 = {
     _id: new Date().toISOString(),
     user: 'spiderman',
     mensaje: 'Mi tía hizo unos panqueques muy buenos',
@@ -14,13 +14,26 @@ let mensaje = {
 };
 
 // 2- Insertar en la base de datos
-db.put(mensaje)
-    .then(console.log('Posteado el mensaje con éxito'))
-    .catch(console.log);
+// db.put(mensaje1)
+//     .then(console.log('Posteado el mensaje con éxito'))
+//     .catch(console.log);
 
 // 3- Leer todos los mensajes offline
 // que aparezcan en la consola
+const añadirMensaje = (heroe, mensaje) => {
+    const objeto = {
+        _id: new Date().toISOString(),
+        user: heroe,
+        mensaje: mensaje,
+        sincronizado: false
+    }
 
+    db.put(objeto)
+        .then(console.log(objeto.mensaje))
+        .catch(console.log);
+};
+
+const mensaje2 = añadirMensaje("Ironman", "Trabajando en el mk50");
 
 
 // 4- Cambiar el valor 'sincronizado' de todos los objetos
